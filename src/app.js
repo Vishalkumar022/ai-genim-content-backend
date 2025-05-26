ce7ce7fa408e1a6c182d6be43d3592b858760281
Vishal Kumar <thingswaste850@gmail.com>
2025-05-26 10:59:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-
+app.get("/ping", (req, res) => {
+    res.status(200).json({ success: true, message: "Server is live!" })
+})
 
 //routes import
 import userRouter from './routes/user.route.js'
